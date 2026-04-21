81a1635d0c64ccba5e7a11573b4c32412ab0842f
GabrielBorges240 <gabrieuborge@gmail.com>
2026-04-21 14:15:52 -0400

    ajusta conexão com banco

--- api-universidade/database.py
+++ api-universidade/database.py
@@ -1,9 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Senha vazia do XAMPP
-DATABASE_URL = "mysql+pymysql://root:@localhost/universidade"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./universidade.db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
